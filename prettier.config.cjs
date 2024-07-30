4f68da71ebb772902615c9f1407d0102cf9e8f2f
amirtbi <torabi46@gmail.com>
2024-07-30 22:40:52 +0330

    add prettier tailwindcss config

---
+++ prettier.config.cjs
@@ -0,0 +1,6 @@
+  module.exports = {
+    // eslint-disable-next-line no-undef
+    plugins: [require("prettier-plugin-tailwindcss")],
+    tailwindConfig: "./tailwind.config.js",
+    singleQoute:true,
+  }
--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
 function Home() {
-
-
   return (
     <>
       <div>
-        <h1 className="text-2xl">
+        <h1 className="text-center text-2xl font-semibold text-purple-400">
           The best pizza.
           <br />
           Straight out of the oven, straight to you.
         </h1>
-        <div style={{display:"flex",gap:"4px"}}>
+        <div style={{ display: "flex", gap: "4px" }}>
           <Link to="/menu">Menu</Link>
           <Link to="/cart">Cart</Link>
           <Link to="/order">Order</Link>
