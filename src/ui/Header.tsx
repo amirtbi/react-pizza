f2d5ec436f318e233debc59f715d4caade4abea1
amirtbi <torabi46@gmail.com>
2024-07-30 22:51:13 +0330

    Define some classes

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import { SearchOrder } from "../faetures/order/SearchOrder";
 
-export function Header(){
-    return <>
+export function Header() {
+  return (
+    <>
+      <header className="bg-yellow-500">
         <Link to="/">Home</Link>
-        <SearchOrder/>
+        <SearchOrder />
         <p>Pizza react app</p>
+      </header>
     </>
+  );
 }
--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -3,10 +3,12 @@ function Home() {
   return (
     <>
       <div>
-        <h1 className="text-center text-2xl font-semibold text-purple-400">
+        <h1 className="text-center text-xl font-semibold text-stone-700">
           The best pizza.
           <br />
-          Straight out of the oven, straight to you.
+          <span className="text-yellow-500">
+            Straight out of the oven, straight to you.
+          </span>
         </h1>
         <div style={{ display: "flex", gap: "4px" }}>
           <Link to="/menu">Menu</Link>
