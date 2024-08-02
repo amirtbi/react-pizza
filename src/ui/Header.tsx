30b46194e254af0780684ce744f277ddce18dcf4
amirtbi <torabi46@gmail.com>
2024-08-02 14:35:08 +0330

    add reponsive classes

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -5,7 +5,7 @@ import { Username } from "../faetures/user/Username";
 export function Header() {
   return (
     <>
-      <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
         <Link to="/" className="tracking-widest">
           Fast react Pizza Co.
         </Link>
